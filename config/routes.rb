49fe14553f078ec902784855fbcd42b5653650ce
Mattens15 <matteo.attenni@outlook.it>
2018-05-22 11:06:08 +0200

    predisposizione form di signup e pagina profilo utente

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#signup'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
