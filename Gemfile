3c00b71395cde52ce86e962ef4e5ae8016e600eb
Mattens15 <matteo.attenni@outlook.it>
2018-05-27 22:58:54 +0200

    fix uniqueness username + matching password confirmation

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'haml-rails'
 gem 'bootstrap'
+gem 'omniauth'
+gem 'omniauth-facebook'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
     before_save { email.downcase! }
-    validates :username, presence: true, length: { maximum: 50 }
+    validates :username, presence: true, length: { maximum: 50 }, uniqueness: true;
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence:   true,
                       format:     { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :password, length: { minimum: 6 }
+    validates :password, length: { minimum: 8,maximum:15 }, confirmation: {case_sensitive:true}; 
+    validates :password_confirmation, presence: true;
 end
