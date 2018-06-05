9781e640c685113c752f9625b8211a5fe50245b0
DanieleFedeli <danieligno10@gmail.com>
2018-06-05 20:40:15 +0200

    Aggiunto jquery ujs nei nodemodules, ajax call per prenotarsi ad una room. Test in corso per i controller

--- db/migrate/20180528233547_create_reservations.rb
+++ db/migrate/20180528233547_create_reservations.rb
@@ -3,7 +3,8 @@ class CreateReservations < ActiveRecord::Migration[5.2]
     create_table :reservations do |t|
       t.references :user, foreign_key: true
       t.references :room, foreign_key: true
-
+      
+      
       t.timestamps
     end
     add_index :reservations, [:user_id, :room_id], unique: true
--- spec/controllers/reservation_controller_spec.rb
+++ spec/controllers/reservation_controller_spec.rb
@@ -7,12 +7,17 @@ RSpec.describe ReservationsController, type: :controller do
         user = User.new(:username => 'Marco', :email => 'marco@gmail.com', :password => '12341234', :password_confirmation => '12341234')
         expect(user.save!).to be true
         
+        
         room = user.rooms.build(:name => 'Abaco', :max_participants => 1, :time_from => '2018-06-19 19:10', :time_to => '2018-06-19 22:10')
         expect(room.save!).to be true
+        params = {room_id: room.id}
         
         allow(controller).to receive(:current_user).and_return(user);
-        post :create, id: @room.id
-        expect(user.reservations.exists?(room_id: @room.id)).to be true
+        expect{post :create, 
+              xhr: true,
+              params: params, 
+              format: :js
+        }.to change { user.reservations.reload.count }.by(1)
       end
     end
   end
