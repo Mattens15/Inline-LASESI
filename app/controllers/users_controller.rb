d84f0d086b02920b8167054434307647e55440ac
Mattens15 <matteo.attenni@outlook.it>
2018-09-18 14:50:43 +0200

    fix logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    
     if @user.save
+      @user.activated=true
+      @user.save
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account"
       redirect_to root_url
